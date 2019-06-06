76d08d8e71a63002a63d95bb5ccf01a0c7cd60bd
lidkapro <44070274+lidkapro@users.noreply.github.com>
2019-06-06 22:33:02 +0300

    log out fixed

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,30 +1,38 @@
 import React from 'react'
 import {Button, Icon, Menu} from 'antd'
 import {withRouter} from 'react-router-dom'
-
+import axios from 'axios'
 
 export const NavMenu = ({children, history, location}) =>
     <section>
-        <header className='main_header'>
-            <Menu
-                onClick={e => history.push(`/${e.key}`)}
-                selectedKeys={[location.pathname.split('/')[1]]}
-                mode="horizontal"
-            >
-                <Menu.Item key='users'>
-                    <Icon type="user"/>Users
-                </Menu.Item>
-                <Menu.Item key='groups'>
-                    <Icon type="team"/>Groups
-                </Menu.Item>
-                <Menu.Item key='authorities'>
-                    <Icon type="team"/>Authorities
-                </Menu.Item>
-            </Menu>
-            <Button onClick={() => window.location = '/logout'} style={{marginRight: 10}}>Log out</Button>
-        </header>
-        {children}
-    </section>
+            <header className='main_header'>
+                <Menu
+                    onClick={e => history.push(`/${e.key}`)}
+                    selectedKeys={[location.pathname.split('/')[1]]}
+                    mode="horizontal"
+                >
+                    <Menu.Item key='users'>
+                        <Icon type="user"/>Users
+                    </Menu.Item>
+                    <Menu.Item key='groups'>
+                        <Icon type="team"/>Groups
+                    </Menu.Item>
+                    <Menu.Item key='authorities'>
+                        <Icon type="team"/>Authorities
+                    </Menu.Item>
+                </Menu>
+                <form action="/logout"
+                      method="post">
+                    <Button htmlType="submit" style={{marginRight: 10}}>Log out</Button>
+
+                    <input type="hidden"
+                           name="${_csrf.parameterName}"
+                           value="${_csrf.token}"/>
+                </form>
+            </header>
+            {children}
+        </section>
+
 
 NavMenu.propTypes = {}
 
